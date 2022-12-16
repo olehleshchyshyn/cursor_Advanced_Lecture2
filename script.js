091ad6cdf8f58044400b7967461553cc19f94936
olehleshchyshyn <112932603+olehleshchyshyn@users.noreply.github.com>
2022-12-16 15:52:58 +0200

    homework
    homework

---
+++ script.js
@@ -0,0 +1,47 @@
+let num = 0;
+while (num < 5) {
+    console.log(num);
+  num++;
+}
+
+
+// let arg = prompt("Введіть значення?");
+// switch (arg) {
+//   case '0':
+//   case '1':
+//     alert( 'Один або нуль' );
+//     break;
+
+//   case '2':
+//     alert( 'Два' );
+//     break;
+
+//   case 3:
+//     alert( 'Ніколи не буде виконано!' );
+//     break;
+//   default:
+//     alert( 'Невідоме значення' );
+// } m
+
+
+
+// let accessAllowed;
+// let age = prompt('Скільки вам років?', '');
+
+// if (age > 18) {
+//   accessAllowed = true;
+// } else {
+//   accessAllowed = false;
+// }
+
+// alert(accessAllowed);
+
+
+
+let year = prompt('Який зараз рік?', '');
+
+if (year == 2022) {
+    alert( 'Правильно!' );
+  } else {
+    alert( 'НЄЄЄЄ' ); 
+}
